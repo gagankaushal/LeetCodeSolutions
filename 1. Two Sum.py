2baf26ab69933535f1e02a043b9e0cbb3e1f9b60
gagankaushal <gagankaushal@users.noreply.github.com>
2019-04-29 11:48:59 +0530

    Add files via upload

---
+++ 1. Two Sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        ans = []
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                
+                if  nums[i] + nums [j] == target:
+                    ans.append(i)
+                    ans.append(j)
+                    break
+        
+        return(ans)        
+
+A=Solution()
+A.twoSum([3,3], 6)
