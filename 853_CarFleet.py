efddd6213f623e7c65f5ec81608f48be18fccf51
Gagan <gagankaushal@users.noreply.github.com>
2021-01-16 17:28:24 -0500

    Time: O(nlogn), Space: O(n)

---
+++ 853_CarFleet.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars  = sorted(zip(position, speed))
+        # print(cars) s = d/t   t = d/s
+        times = [(target-p)/s for p,s in cars]
+        print(times)
+        ans = 0
+        while len(times)>1:
+            lead = times.pop()
+            if lead<times[-1]:
+                ans += 1
+            else:
+                times[-1]=lead
+        return ans + bool(times)
